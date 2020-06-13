cd941b158069058f13d284a8032d6ff9ccdebde8
pocari <caffelattenonsugar@gmail.com>
2020-06-13 17:28:07 +0900

    callbackのrouting追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #
+  #Rails.application.routes.draw do
+  get 'auth/auth0/callback' => 'auth0#callback'
+  get 'auth/failure' => 'auth0#failure'
+end
 end
